8c53fb384fc1e4b2bf1e9149cace12ec6a2636f8
inkblast <ies.imeshnuwanthak@gmail.com>
2025-03-07 15:02:28 +0530

    updated shopify configarations

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -6,6 +6,9 @@ import {
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
